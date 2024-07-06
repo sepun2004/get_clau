e626d480879984761afaf7c46d6877fed0c36ee3
sepun2004 <145572771+sepun2004@users.noreply.github.com>
2024-07-06 19:27:41 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: czamora- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 16:11:22 by czamora-          #+#    #+#             */
+/*   Updated: 2024/07/06 19:14:53 by czamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin_and_free(char *stored, char *buf);
+char	*read_and_store(int fd, char *stored);
+char	*extract_line(char **stored);
+char	*get_next_line(int fd);
+
+#endif
